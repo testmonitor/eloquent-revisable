16c0dac6ee62eccca38bc10fc494cef363e48053
Thijs Kok <tkok@testmonitor.com>
2026-03-27 09:48:01 +0100

    Add ConfigurationTest to validate revision and name generator configurations

---
+++ tests/ConfigurationTest.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace TestMonitor\Revisable\Tests;
+
+use PHPUnit\Framework\Attributes\Test;
+use TestMonitor\Revisable\Exceptions\InvalidConfiguration;
+use TestMonitor\Revisable\Generators\NameGeneratorFactory;
+use TestMonitor\Revisable\Generators\VersionNameGenerator;
+use TestMonitor\Revisable\Models\Revision;
+use TestMonitor\Revisable\RevisableServiceProvider;
+use TestMonitor\Revisable\Tests\Models\Author;
+
+class ConfigurationTest extends TestCase
+{
+    // Revision model
+
+    #[Test]
+    public function it_returns_the_default_revision_model_class()
+    {
+        // When
+        $revisionModel = RevisableServiceProvider::determineRevisionModel();
+
+        // Then
+        $this->assertEquals(Revision::class, $revisionModel);
+    }
+
+    #[Test]
+    public function it_throws_when_the_configured_revision_model_does_not_extend_the_base_revision()
+    {
+        // Given
+        config()->set('revisable.revision_model', \stdClass::class);
+
+        // When / Then
+        $this->expectException(InvalidConfiguration::class);
+
+        RevisableServiceProvider::determineRevisionModel();
+    }
+
+    #[Test]
+    public function it_throws_when_the_configured_revision_model_class_does_not_exist()
+    {
+        // Given
+        config()->set('revisable.revision_model', 'App\\Models\\NonExistentRevision');
+
+        // When / Then
+        $this->expectException(InvalidConfiguration::class);
+
+        RevisableServiceProvider::determineRevisionModel();
+    }
+
+    // Name generator
+
+    #[Test]
+    public function it_returns_null_when_no_name_generator_is_configured()
+    {
+        // Given
+        config()->set('revisable.name_generator', null);
+
+        // When
+        $generator = NameGeneratorFactory::create();
+
+        // Then
+        $this->assertNull($generator);
+    }
+
+    #[Test]
+    public function it_creates_a_generator_instance_when_the_class_is_valid()
+    {
+        // Given
+        config()->set('revisable.name_generator', VersionNameGenerator::class);
+
+        // When
+        $generator = NameGeneratorFactory::create();
+
+        // Then
+        $this->assertInstanceOf(VersionNameGenerator::class, $generator);
+    }
+
+    #[Test]
+    public function it_throws_when_the_configured_name_generator_does_not_implement_the_contract()
+    {
+        // Given
+        config()->set('revisable.name_generator', \stdClass::class);
+
+        // When / Then
+        $this->expectException(InvalidConfiguration::class);
+
+        NameGeneratorFactory::create();
+    }
+
+    #[Test]
+    public function it_throws_when_the_configured_name_generator_class_does_not_exist()
+    {
+        // Given
+        config()->set('revisable.name_generator', 'App\\Generators\\NonExistentGenerator');
+
+        // When / Then
+        $this->expectException(InvalidConfiguration::class);
+
+        NameGeneratorFactory::create();
+    }
+
+    // User model
+
+    #[Test]
+    public function it_returns_the_configured_user_model_class()
+    {
+        // Given
+        config()->set('revisable.user_model', Author::class);
+
+        // When
+        $userModel = RevisableServiceProvider::determineUserModel();
+
+        // Then
+        $this->assertEquals(Author::class, $userModel);
+    }
+
+    #[Test]
+    public function it_throws_when_the_user_model_is_not_a_valid_model_class()
+    {
+        // Given
+        config()->set('revisable.user_model', \stdClass::class);
+
+        // When / Then
+        $this->expectException(InvalidConfiguration::class);
+
+        RevisableServiceProvider::determineUserModel();
+    }
+}
