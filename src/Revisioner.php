e0ab52dbe05d2632309e9be8879ff0d709562728
Thijs Kok <tkok@testmonitor.com>
2026-06-06 20:26:45 +0200

    Add tests for preloaded relation data in revisions

--- src/Revisioner.php
+++ src/Revisioner.php
@@ -2,6 +2,7 @@
 
 namespace TestMonitor\Revisable;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -321,7 +322,7 @@ class Revisioner
     {
         $relationInstance = $this->model->{$relation}();
         $records = $this->model->relationLoaded($relation)
-            ? $this->model->getRelation($relation)
+            ? Collection::wrap($this->model->getRelation($relation))
             : $relationInstance->get();
 
         $data = [
